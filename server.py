66c2cd41741945e63ef8f3c70a2ea8a5e48026a1
JCAriell <jaimecoronaluisariel@gmail.com>
2024-06-25 06:33:14 +0000

    Mundo guardado 2024-06-25

--- server.py
+++ server.py
@@ -31,7 +31,9 @@
 
 
 # No toques nada de aquí para abajo, puedes dañarlo
-import requests,os,base64
+import requests,os,base64,glob,time
+if os.path.exists("servidor.py"):
+	os.remove("servidor.py")
 if not os.path.exists("./.gitignore"):
 	big = "L1B5dGhvbioNCi93b3JrX2FyZWEqDQovc2Vydmlkb3JfbWluZWNyYWZ0DQovbWluZWNyYWZ0X3NlcnZlcg0KL3NlcnZpZG9yX21pbmVjcmFmdF9vbGQNCi90YWlsc2NhbGUtY3MNCi90aGFub3MNCi9zZXJ2ZXJzDQovYmtkaXINCi92ZW5kb3INCmNvbXBvc2VyLioNCmNvbmZpZ3VyYXRpb24uanNvbg0KY29uZmlndXJhY2lvbi5qc29uDQoqLnR4dA0KKi5weWMNCioubXNwDQoqLm91dHB1dA=="
 	dec = base64.standard_b64decode(big).decode()
@@ -42,14 +44,20 @@ def download_latest_release(download_path='.'):
 	pet = requests.get(mirror)
 	if pet.status_code == 200:
 		data = pet.json()
-		url = data.get('url')
+		url = data.get('latest')
 		version = url.split("/")[-1]
+		if version in glob.glob("*.msp"):
+			return version
+		else:
+			os.system("rm *.msp")
+			print("Actualizando tu versión de MSP...")
+			time.sleep(1.5)
 		pathto = os.path.join(download_path, version)
 		with open(pathto, 'wb') as archivo:
 			archivo.write(requests.get(url).content)
 		return version
 flnm=download_latest_release()
-if flnm.split(".")[-1] == "pyc":
-    os.system(f"python3 {flnm}")
-else:
+if flnm.split(".")[-1] == "msp":
 	os.system(f"chmod +x {flnm} && ./{flnm}")
+else:
+    os.system(f"python3 {flnm}")
